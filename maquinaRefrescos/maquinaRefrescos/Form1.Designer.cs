c687dfd00c43bdff83f8de104b013cf2ea60dee4
rodrigocor <73333833+rodrigocor@users.noreply.github.com>
2022-11-04 01:55:43 +0100

    Add files via upload
    Ejercicios windows form máquina de refrescos

---
+++ maquinaRefrescos/maquinaRefrescos/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace maquinaRefrescos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRndija = new System.Windows.Forms.PictureBox();
+            this.botellaMorada = new System.Windows.Forms.PictureBox();
+            this.botellaAmarilla = new System.Windows.Forms.PictureBox();
+            this.botellaAzul = new System.Windows.Forms.PictureBox();
+            this.botellaRoja = new System.Windows.Forms.PictureBox();
+            this.botellaVerde = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cambio = new System.Windows.Forms.Label();
+            this.monedas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRndija)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.botellaMorada)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.botellaAmarilla)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.botellaAzul)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.botellaRoja)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.botellaVerde)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monedas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(437, 423);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 106);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Introduzca el número del producto, actualmente solo 5 bebidas, 1 numero por colum" +
+    "na";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(444, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.boton1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(465, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(20, 22);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.boton2);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(486, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(20, 22);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.boton3);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Silver;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(444, 293);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(20, 22);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.boton4);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Silver;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Location = new System.Drawing.Point(486, 293);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(20, 22);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.boton5);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Silver;
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button6.Location = new System.Drawing.Point(486, 333);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(20, 22);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "X";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Silver;
+            this.button7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button7.Location = new System.Drawing.Point(444, 333);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(20, 22);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "€";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(452, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 55);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(450, 368);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 51);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.calcular);
+            // 
+            // pictureBoxRndija
+            // 
+            this.pictureBoxRndija.Image = global::maquinaRefrescos.Properties.Resources.rendija;
+            this.pictureBoxRndija.Location = new System.Drawing.Point(56, 694);
+            this.pictureBoxRndija.Name = "pictureBoxRndija";
+            this.pictureBoxRndija.Size = new System.Drawing.Size(358, 88);
+            this.pictureBoxRndija.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxRndija.TabIndex = 12;
+            this.pictureBoxRndija.TabStop = false;
+            // 
+            // botellaMorada
+            // 
+            this.botellaMorada.Image = global::maquinaRefrescos.Properties.Resources.bebidaMorada;
+            this.botellaMorada.Location = new System.Drawing.Point(193, 712);
+            this.botellaMorada.Name = "botellaMorada";
+            this.botellaMorada.Size = new System.Drawing.Size(42, 70);
+            this.botellaMorada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.botellaMorada.TabIndex = 13;
+            this.botellaMorada.TabStop = false;
+            // 
+            // botellaAmarilla
+            // 
+            this.botellaAmarilla.Image = global::maquinaRefrescos.Properties.Resources.bebidaAmarilla1;
+            this.botellaAmarilla.Location = new System.Drawing.Point(116, 712);
+            this.botellaAmarilla.Name = "botellaAmarilla";
+            this.botellaAmarilla.Size = new System.Drawing.Size(71, 41);
+            this.botellaAmarilla.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.botellaAmarilla.TabIndex = 14;
+            this.botellaAmarilla.TabStop = false;
+            // 
+            // botellaAzul
+            // 
+            this.botellaAzul.Image = global::maquinaRefrescos.Properties.Resources.bebidaAzul;
+            this.botellaAzul.Location = new System.Drawing.Point(270, 742);
+            this.botellaAzul.Name = "botellaAzul";
+            this.botellaAzul.Size = new System.Drawing.Size(69, 40);
+            this.botellaAzul.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.botellaAzul.TabIndex = 15;
+            this.botellaAzul.TabStop = false;
+            // 
+            // botellaRoja
+            // 
+            this.botellaRoja.Image = global::maquinaRefrescos.Properties.Resources.bebidaRoja;
+            this.botellaRoja.Location = new System.Drawing.Point(361, 712);
+            this.botellaRoja.Name = "botellaRoja";
+            this.botellaRoja.Size = new System.Drawing.Size(40, 69);
+            this.botellaRoja.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.botellaRoja.TabIndex = 16;
+            this.botellaRoja.TabStop = false;
+            // 
+            // botellaVerde
+            // 
+            this.botellaVerde.Image = global::maquinaRefrescos.Properties.Resources.bebidaVerde;
+            this.botellaVerde.Location = new System.Drawing.Point(69, 702);
+            this.botellaVerde.Name = "botellaVerde";
+            this.botellaVerde.Size = new System.Drawing.Size(41, 68);
+            this.botellaVerde.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.botellaVerde.TabIndex = 17;
+            this.botellaVerde.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(437, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(77, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // cambio
+            // 
+            this.cambio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.cambio.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cambio.ForeColor = System.Drawing.Color.Yellow;
+            this.cambio.Location = new System.Drawing.Point(452, 541);
+            this.cambio.Name = "cambio";
+            this.cambio.Size = new System.Drawing.Size(46, 31);
+            this.cambio.TabIndex = 19;
+            this.cambio.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // monedas
+            // 
+            this.monedas.Image = global::maquinaRefrescos.Properties.Resources.cambio;
+            this.monedas.Location = new System.Drawing.Point(458, 575);
+            this.monedas.Name = "monedas";
+            this.monedas.Size = new System.Drawing.Size(33, 33);
+            this.monedas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.monedas.TabIndex = 20;
+            this.monedas.TabStop = false;
+            this.monedas.Click += new System.EventHandler(this.recogerCambio);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(558, 905);
+            this.Controls.Add(this.monedas);
+            this.Controls.Add(this.cambio);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.botellaVerde);
+            this.Controls.Add(this.botellaRoja);
+            this.Controls.Add(this.botellaAzul);
+            this.Controls.Add(this.botellaAmarilla);
+            this.Controls.Add(this.botellaMorada);
+            this.Controls.Add(this.pictureBoxRndija);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Máquina de refrescos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRndija)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.botellaMorada)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.botellaAmarilla)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.botellaAzul)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.botellaRoja)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.botellaVerde)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monedas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxRndija;
+        private System.Windows.Forms.PictureBox botellaMorada;
+        private System.Windows.Forms.PictureBox botellaAmarilla;
+        private System.Windows.Forms.PictureBox botellaAzul;
+        private System.Windows.Forms.PictureBox botellaRoja;
+        private System.Windows.Forms.PictureBox botellaVerde;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label cambio;
+        private System.Windows.Forms.PictureBox monedas;
+    }
+}
+
---
+++ maquinaRefrescos/maquinaRefrescos/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace maquinaRefrescos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            monedas.Hide();
+            pictureBoxRndija.Hide();
+            botellaAmarilla.Hide();
+            botellaAzul.Hide();
+            botellaMorada.Hide();
+            botellaRoja.Hide();
+            botellaVerde.Hide();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton1(object sender, EventArgs e)
+        {
+            label2.Text = "1,15";
+            label1.Text = "Escriba el dinero que quiere ingresar en el cuadro blanco, y haga click en la palanca";
+        }
+        private void boton2(object sender, EventArgs e)
+        {
+            label2.Text = "2";
+            label1.Text = "Escriba el dinero que quiere ingresar en el cuadro blanco, y haga click en la palanca";
+        }
+        private void boton3(object sender, EventArgs e)
+        {
+            label2.Text = "0,50";
+            label1.Text = "Escriba el dinero que quiere ingresar en el cuadro blanco, y haga click en la palanca";
+        }
+        private void boton4(object sender, EventArgs e)
+        {
+            label2.Text = "3";
+            label1.Text = "Escriba el dinero que quiere ingresar en el cuadro blanco, y haga click en la palanca";
+        }
+        private void boton5(object sender, EventArgs e)
+        {
+            label2.Text = "1";
+            label1.Text = "Escriba el dinero que quiere ingresar en el cuadro blanco, y haga click en la palanca";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcular(object sender, EventArgs e)
+        {
+            double precio=0;
+            double dineroIntroducido=0;
+            double res = 0;
+            if (string.IsNullOrEmpty(label2.Text))
+            {
+                label1.Text="No ha introducido dinero, introduzca la cantidad que deseé";
+            }
+
+            if (!string.IsNullOrEmpty(label2.Text)){
+                precio = double.Parse(label2.Text);
+                dineroIntroducido = double.Parse(textBox1.Text);
+                res = dineroIntroducido - precio;
+                if (res  >= 0.0)
+                {
+
+                    if (label2.Text == "1,15")
+                    {
+                        botellaAmarilla.Show();
+                        
+                        pictureBoxRndija.Show();
+                        botellaAmarilla.BringToFront();
+                    }
+
+                    if (label2.Text == "2")
+                    {
+                        botellaMorada.Show();
+                        pictureBoxRndija.Show();
+                        botellaMorada.BringToFront();
+                    }
+
+                    if (label2.Text == "0,50")
+                    {
+                        botellaVerde.Show();
+                        pictureBoxRndija.Show();
+                        botellaVerde.BringToFront();
+                    }
+
+                    if (label2.Text == "3")
+                    {
+                        botellaRoja.Show();
+                        pictureBoxRndija.Show();
+                        botellaRoja.BringToFront();
+                    }
+
+                    if (label2.Text == "1")
+                    {
+                        botellaAzul.Show();
+                        pictureBoxRndija.Show();
+                        botellaAzul.BringToFront();
+                    }
+                    if(res >= 0.0)
+                    {
+                        label1.Text = "Recoja su cambio abajo, clickando";
+                        cambio.Text = res.ToString();
+                        monedas.Show();
+
+                    }
+                    
+                }
+                else
+                {
+                    label1.Text = "Dinero insuficiente, Recoja su cambio abajo, clickando;";
+     
+                    cambio.Text = dineroIntroducido.ToString();
+                    monedas.Show();
+                }
+
+
+            }
+        }
+
+        private void recogerCambio(object sender, EventArgs e)
+        {
+            label1.Text = "Introduzca el número del producto, actualmente solo 5 bebidas, 1 numero por columna";
+            monedas.Hide();
+            cambio.Text = "";
+            label2.Text = "0";
+            textBox1.Text = "";
+             
+            pictureBoxRndija.Hide();
+            botellaAmarilla.Hide();
+            botellaAzul.Hide();
+            botellaMorada.Hide();
+            botellaRoja.Hide();
+            botellaVerde.Hide();
+        }
+
+        //private void 
+    }
+}
