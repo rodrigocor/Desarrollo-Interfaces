28ef21bf2b128948fcc198a1744ace17a18d9c6a
rodrigocor <73333833+rodrigocor@users.noreply.github.com>
2022-11-04 02:01:16 +0100

    Add files via upload
    Ejercicio windows form suma de 1 a n

---
+++ SumasDe1aN/SumasDe1aN/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SumasDe1aN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxSumando = new System.Windows.Forms.ListBox();
+            this.Iniciar = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calcular la suma de 1 hasta:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBoxSumando
+            // 
+            this.listBoxSumando.FormattingEnabled = true;
+            this.listBoxSumando.Location = new System.Drawing.Point(51, 111);
+            this.listBoxSumando.Name = "listBoxSumando";
+            this.listBoxSumando.Size = new System.Drawing.Size(258, 199);
+            this.listBoxSumando.TabIndex = 1;
+            this.listBoxSumando.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Iniciar
+            // 
+            this.Iniciar.Location = new System.Drawing.Point(142, 353);
+            this.Iniciar.Name = "Iniciar";
+            this.Iniciar.Size = new System.Drawing.Size(75, 23);
+            this.Iniciar.TabIndex = 2;
+            this.Iniciar.Text = "Iniciar";
+            this.Iniciar.UseVisualStyleBackColor = true;
+            this.Iniciar.Click += new System.EventHandler(this.Iniciar_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(194, 50);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(51, 76);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(141, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Ver resultados parciales ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.Iniciar);
+            this.Controls.Add(this.listBoxSumando);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sumas de 1 a N";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxSumando;
+        private System.Windows.Forms.Button Iniciar;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ SumasDe1aN/SumasDe1aN/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumasDe1aN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var tt1 = new ToolTip();
+            tt1.SetToolTip(Iniciar, "Haga click para calcular");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Iniciar_Click(object sender, EventArgs e)
+        {
+            listBoxSumando.Items.Clear();
+            int parcial = 0;
+            for (int i=0;i< numericUpDown1.Value;i++)
+            {
+                
+                if (checkBox1.Checked)
+                {
+
+                    listBoxSumando.Items.Add("sumando: " + (i + 1) + " Suma Parcial: " + ((i+1)+parcial));
+                    parcial=i+1 + parcial;
+                }
+                else
+                {
+                    listBoxSumando.Items.Add("sumando: " + (i+1) );
+                    parcial = i + 1 + parcial;
+                }
+                
+            }
+
+                listBoxSumando.Items.Add("La suma TOTAL es: " + parcial);
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
