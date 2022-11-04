8ecec9e774da70d2b998c63c060fcc7bc87a8b22
rodrigocor <73333833+rodrigocor@users.noreply.github.com>
2022-11-04 01:59:53 +0100

    Add files via upload
    Ejercicios windows form suma de dos números

---
+++ sumaDeDosNumeros/sumaDeDosNumeros/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace sumaDeDosNumeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numeroUno = new System.Windows.Forms.MaskedTextBox();
+            this.numeroDos = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // numeroUno
+            // 
+            this.numeroUno.Location = new System.Drawing.Point(15, 103);
+            this.numeroUno.Name = "numeroUno";
+            this.numeroUno.Size = new System.Drawing.Size(107, 20);
+            this.numeroUno.TabIndex = 0;
+            this.numeroUno.ValidatingType = typeof(int);
+            this.numeroUno.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.numeroUno_MaskInputRejected);
+            // 
+            // numeroDos
+            // 
+            this.numeroDos.Location = new System.Drawing.Point(231, 103);
+            this.numeroDos.Name = "numeroDos";
+            this.numeroDos.Size = new System.Drawing.Size(107, 20);
+            this.numeroDos.TabIndex = 1;
+            this.numeroDos.ValidatingType = typeof(int);
+            this.numeroDos.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox2_MaskInputRejected);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::sumaDeDosNumeros.Properties.Resources.suma1;
+            this.button1.Location = new System.Drawing.Point(149, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 59);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // resultado
+            // 
+            this.resultado.Location = new System.Drawing.Point(120, 188);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(113, 20);
+            this.resultado.TabIndex = 3;
+            this.resultado.TextChanged += new System.EventHandler(this.resultado_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Haga click en la suma para calcular";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Introduzca un número";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Introduzca un número";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 244);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numeroDos);
+            this.Controls.Add(this.numeroUno);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Suma de dos números";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox numeroUno;
+        private System.Windows.Forms.MaskedTextBox numeroDos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox resultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ sumaDeDosNumeros/sumaDeDosNumeros/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sumaDeDosNumeros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double res;
+            res = double.Parse(numeroUno.Text) + double.Parse(numeroDos.Text);
+            resultado.Text = res.ToString();
+        }
+
+        private void numeroUno_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void resultado_TextChanged(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
